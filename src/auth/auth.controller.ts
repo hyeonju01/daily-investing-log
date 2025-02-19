aa5e3742ecf4569242bc258d76c65744db30fd81
hyeonju01 <tangled6648@gmail.com>
2025-02-20 00:56:36 +0900

    feat: 회원가입, 로그인 기능 구현

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,8 +1,24 @@
-import { Controller } from '@nestjs/common';
-import {AuthService} from "./auth.service";
+import { Body, Controller, Post } from '@nestjs/common'
+import { AuthService } from './auth.service'
+import { UsersService } from '../users/users.service'
+import { CreateUserDto } from '../users/dto/create-user.dto'
+import { LoginUserDto } from '../users/dto/login-user.dto'
 
-@Controller('/api/auth')
+@Controller('/api/users/auth')
 export class AuthController {
-    constructor(private authService: AuthService) {
-    }
+  constructor(
+    private authService: AuthService,
+    private usersService: UsersService,
+  ) {}
+
+  @Post('/signUp')
+  async signUp(@Body() createUserDto: CreateUserDto) {
+    return this.usersService.signUp(createUserDto)
+  }
+  @Post('/signIn')
+  async signIn(@Body() loginUserDto: LoginUserDto) {
+    /* refresh token을 쿠키에 저장하는 로직 작성*/
+
+    return this.usersService.signIn(loginUserDto)
+  }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,52 +1,103 @@
-import {BadRequestException, Injectable} from '@nestjs/common';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
-import {InjectRepository} from "@nestjs/typeorm";
-import {User} from "./entities/user.entity";
-import {Repository} from "typeorm";
-import * as bcrypt from "bcrypt";
+import { BadRequestException, Injectable } from '@nestjs/common'
+import { CreateUserDto } from './dto/create-user.dto'
+import { UpdateUserDto } from './dto/update-user.dto'
+import { InjectRepository } from '@nestjs/typeorm'
+import { User } from './entities/user.entity'
+import { Repository } from 'typeorm'
+import * as bcrypt from 'bcrypt'
+import { LoginUserDto } from './dto/login-user.dto'
+import { AuthService } from '../auth/auth.service'
 
 @Injectable()
 export class UsersService {
   constructor(
-      @InjectRepository(User) private UserRepository: Repository<User>,
+    @InjectRepository(User)
+    private UserRepository: Repository<User>,
+    private AuthService: AuthService,
   ) {}
 
   // 회원가입
-  async create(createUserDto: CreateUserDto): Promise<User> {
-
-    const { email, password } = createUserDto;
+  async signUp(createUserDto: CreateUserDto): Promise<User> {
+    const { email, password } = createUserDto
     if (!email || !password) {
-      throw new BadRequestException("Email and password is required");
+      throw new BadRequestException('Email and password is required')
     }
 
-    const hashedPass = await this.hashPassword(password);
+    // 이메일 중복 확인
+    const existingUser = await this.UserRepository.findOne({
+      where: { email: email },
+    })
+    if (existingUser) {
+      throw new BadRequestException('Email already exits with existing user')
+    }
 
-    const newUser = this.UserRepository.create({email, password: hashedPass});
-    const createdUser = await this.UserRepository.save(newUser);
+    // 비밀번호 해싱
+    const hashedPass = await this.hashPassword(password)
 
-    return createdUser;
+    const newUser = this.UserRepository.create({ email, password: hashedPass })
+    const createdUser = await this.UserRepository.save(newUser)
+
+    /* 토큰 발급 로직 추가 */
+    const accessToken = this.AuthService.issueAccessToken(
+      createdUser.id,
+      createdUser.email,
+    )
+    const refreshToken = this.AuthService.issueRefreshToken(createdUser.id)
+
+    return createdUser
+  }
+
+  // 로그인
+  async signIn(loginUserDto: LoginUserDto): Promise<string> {
+    // 이메일 검증
+    const user = await this.UserRepository.findOne({
+      where: { email: loginUserDto.email },
+    })
+
+    if (!user || !user.email) {
+      throw new BadRequestException('Email and password is required')
+    }
+
+    // 비밀번호 검증
+    const comparePassword = await bcrypt.compare(
+      loginUserDto.password,
+      user.password,
+    )
+
+    if (!comparePassword) {
+      throw new BadRequestException('Invalid credentials')
+    }
+
+    // 토큰 발급
+    const accessToken = await this.AuthService.issueAccessToken(
+      user.id,
+      user.email,
+    )
+    const refreshToken = await this.AuthService.issueRefreshToken(user.id)
+
+    // 응답으로 accessToken 전달
+    return `login successful`
   }
 
   findAll() {
-    return `This action returns all users`;
+    return `This action returns all users`
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} user`;
+    return `This action returns a #${id} user`
   }
 
   update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+    return `This action updates a #${id} user`
   }
 
   remove(id: number) {
-    return `This action removes a #${id} user`;
+    return `This action removes a #${id} user`
   }
 
   // 비밀번호 암호화
   async hashPassword(password: string): Promise<string> {
-    const salt = await bcrypt.genSalt(10);
-    return await bcrypt.hash(password, salt);
+    const salt = await bcrypt.genSalt(10)
+    return await bcrypt.hash(password, salt)
   }
 }
