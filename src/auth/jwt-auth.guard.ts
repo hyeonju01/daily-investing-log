204294bd7f5e5a7be9f36abb6d7243238ed8fbec
hyeonju01 <tangled6648@gmail.com>
2025-02-21 01:46:00 +0900

    feat: 인증 구현

---
+++ src/auth/jwt-auth.guard.ts
@@ -0,0 +1,10 @@
+import { AuthGuard } from '@nestjs/passport'
+import { ExecutionContext, Injectable } from '@nestjs/common'
+import { Observable } from 'rxjs'
+
+@Injectable()
+export class JwtAuthGuard extends AuthGuard('jwt') {
+  canActivate(context: ExecutionContext) {
+    return super.canActivate(context)
+  }
+}
