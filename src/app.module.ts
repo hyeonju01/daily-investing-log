c713b073bd1fe1ec61e5021c4742b98c565af6b4
hyeonju01 <tangled6648@gmail.com>
2025-02-22 01:55:50 +0900

    chore: dotenv 패키지로 환경변수 관리 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,7 @@
 // NestJS Main (전체 애플리케이션 관리)
+import { config } from 'dotenv'
+
+config({ path: '.env.test' })
 
 import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
@@ -8,19 +11,19 @@ import { TypeOrmModule } from '@nestjs/typeorm'
 import { getDatabaseConfig } from './config/database.config'
 import { UsersModule } from './users/users.module'
 import { AuthModule } from './auth/auth.module'
-import { AssetsModule } from './assets/assets.module';
-import { InvestingLogsModule } from './investing-logs/investing-logs.module';
+import { AssetsModule } from './assets/assets.module'
+import { InvestingLogsModule } from './investing-logs/investing-logs.module'
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       cache: false,
-      // envFilePath: `.env.${process.env.NODE_ENV || 'test'}`,
-      envFilePath: process.env.NODE_ENV === 'test' ? '.env.test' : '.env',
+      envFilePath: '.env.test',
+      // envFilePath: process.env.NODE_ENV === 'test' ? '.env.test' : '.env',
     }),
     TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
+      imports: [ConfigModule, UsersModule],
       inject: [ConfigService],
       useFactory: getDatabaseConfig,
     }),
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,17 @@
 // NestJS 애플리케이션 시작점
+import { config } from 'dotenv'
 
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+config({ path: '.env.test' })
+
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  console.log(`🚀 NODE_ENV: ${process.env.NODE_ENV}`)
+  console.log(`📌 DATABASE_NAME: ${process.env.DATABASE_NAME}`)
+  console.log(`📌 DATABASE_HOST: ${process.env.DATABASE_HOST}`)
+
+  const app = await NestFactory.create(AppModule)
+  await app.listen(process.env.PORT ?? 3000)
 }
-bootstrap();
+bootstrap()
