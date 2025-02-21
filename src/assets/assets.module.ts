b00750718abc1e3f497c56dbf0c82afb19d2efe2
hyeonju01 <tangled6648@gmail.com>
2025-02-22 01:56:24 +0900

    chore: 누락된 의존성 주입

--- src/assets/assets.module.ts
+++ src/assets/assets.module.ts
@@ -1,8 +1,12 @@
-import { Module } from '@nestjs/common';
-import { AssetsService } from './assets.service';
-import { AssetsController } from './assets.controller';
+import { Module } from '@nestjs/common'
+import { AssetsService } from './assets.service'
+import { AssetsController } from './assets.controller'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { InvestingLog } from '../investing-logs/entities/investing-log.entity'
+import { Asset } from './entities/asset.entity'
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Asset])],
   controllers: [AssetsController],
   providers: [AssetsService],
 })
