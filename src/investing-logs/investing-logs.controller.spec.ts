2be7453ba7d47ad66db106416de782e706bb53c7
hyeonju01 <tangled6648@gmail.com>
2025-02-24 19:36:50 +0900

    test: 투자일지 컨트롤러 테스트 수정 논리삭제 엔드포인트

--- src/investing-logs/investing-logs.controller.spec.ts
+++ src/investing-logs/investing-logs.controller.spec.ts
@@ -14,13 +14,14 @@ describe('InvestingLogsController', () => {
     investingDate: new Date('2024-02-19'),
     createdAt: new Date(),
     updatedAt: new Date(),
+    isDeleted: false,
   }
 
   const mockInvestingLogService = {
     create: jest.fn().mockResolvedValue(mockInvestingLog),
     findAllByUserId: jest.fn().mockResolvedValue([mockInvestingLog]),
     findOne: jest.fn().mockResolvedValue(mockInvestingLog),
-    deleteInvestingLog: jest.fn().mockResolvedValue(undefined),
+    softDelete: jest.fn(),
   }
 
   const mockRequest = {
@@ -90,13 +91,13 @@ describe('InvestingLogsController', () => {
     )
   })
 
-  it('should delete an investing log', async () => {
+  it('should soft delete an investing log', async () => {
     const result = await investingLogsController.deleteInvestingLog(
       mockRequest,
       mockInvestingLog.id,
     )
     expect(result).toBeUndefined()
-    expect(mockInvestingLogService.deleteInvestingLog).toHaveBeenCalledWith(
+    expect(mockInvestingLogService.softDelete).toHaveBeenCalledWith(
       mockInvestingLog.id,
       mockRequest.user.id,
     )
